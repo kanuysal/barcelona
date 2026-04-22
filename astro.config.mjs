50049b5670f61ac1ab1dad7d24e59882a690e03b
kanuysal <serkanuysal@msn.com>
2026-04-22 21:52:09 +0200

    fix: switch to static output for cloudflare pages to resolve 404

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  output: 'server',
-  adapter: cloudflare()
+  output: 'static'
 });
